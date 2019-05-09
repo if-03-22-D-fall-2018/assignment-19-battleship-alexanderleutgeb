7398335379adfd8bc79cd537238d7b1362f47dcb
alexanderleutgeb <alex.leutgeb02@gmail.com>
2019-05-09 11:01:36 +0200

    3 tests working

--- battleship.cpp
+++ battleship.cpp
@@ -11,5 +11,47 @@
  *-----------------------------------------------------------------------------
 */
 #include <stdio.h>
+#include "battleship.h"
+CellContent _my_field[FIELDSIZE][FIELDSIZE];
+CellContent _other_field[FIELDSIZE][FIELDSIZE];
+CellContent _my_guess[FIELDSIZE][FIELDSIZE];
+
+void load_game(){
+  FILE* myShip = fopen("battleship.my", "rw");
+  FILE* otherShip = fopen("battleship.op", "rw");
+
+  for (int i = 0; i < FIELDSIZE; i++) {
+    for (int j = 0; j < FIELDSIZE; j++) {
+      fread(&_my_field[i][j], sizeof(CellContent), 1, myShip);
+      fread(&_other_field[i][j], sizeof(CellContent), 1, otherShip);
+      _my_guess[i][j] = Unknown;
+    }
+  }
+}
+
+CellContent get_shot(int row, int col){
+  if (row >= FIELDSIZE || col >= FIELDSIZE || col < 0 || row < 0) {
+    return OutOfRange;
+  }
+  return _my_field[row][col];
+}
+
+bool shoot(int row, int col){
+  if (row >= FIELDSIZE || col >= FIELDSIZE || col < 0 || row < 0) {
+    return false;
+  }
+  if (_other_field[row][col] == Boat) {
+    _my_guess[row][col] == Boat;
+    return true;
+  }
+  return false;
+}
+
+CellContent get_my_guess(int row, int col){
+  if (row >= FIELDSIZE || col >= FIELDSIZE || col < 0 || row < 0) {
+    return OutOfRange;
+  }
+  return _my_guess[row][col];
+}
 
 #include "battleship.h"
